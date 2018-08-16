f2ce42fbd297ccee3c5999ee3583cd39db42913c
Sean.Rennie <sean.rennie6@gmail.com>
2018-08-16 18:02:55 +0100

    created user model and re-ordered both models to be alphabetical

--- models/event.js
+++ models/event.js
@@ -4,12 +4,16 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const eventSchema = mongoose.Schema({
   createdBy: {type: ObjectId, ref: 'User'}, //hosting user
-  eventTitle: { type: String, required: true },
   category: { type: String, required: true },
-  imageUrl: String,
-  eventDate: { type: Date, required: true },
+  capacity: Number,
+  concluded: Boolean,
   duration: { type: String, required: true },
   description: String,
+  eventTitle: { type: String, required: true },
+  eventDate: { type: Date, required: true },
+  guests: [ { type: ObjectId } ], //to hold all users attending the event
+  imageUrl: String,
+  isIndoors: Boolean,
   location: {     //sub document to hold event location
     streetNumber: Number,
     streetName: String,
@@ -17,16 +21,12 @@ const eventSchema = mongoose.Schema({
     lat: Number,          //we will need to geocode the lat & lon
     lon: Number           // should seed this initially
   },
-  capacity: Number,
-  concluded: Boolean,
-  guests: [ { type: ObjectId } ], //to hold all users attending the event
-  isIndoors: Boolean,
   reviews: {    // sub-document to hold simple reviews
     addedBy: { type: ObjectId },
     rating: { type: Number, required: true },
     comment: String
   }
-});
+}, { timestamps: true });
 
 
 //VIRTUALS
---
+++ models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true },
+  firstName: { type: String, required: true },
+  homeLocation: {
+    lat: Number,
+    lon: Number
+  },
+  isHost: { type: Boolean, default: false },
+  password: { type: String, required: true },
+  surename: { type: String, required: true },
+  username: { type: String, required: true }
+}, { timestamps: true });
+
+
+//Hashed password verification to be used with authController
+userSchema.methods.validatePassword = function(password){
+  //compare the password from the form against the has in the db
+  return bcrypt.compareSync(password, this.password);
+};
+
+
+////////////--VIRTUALS--//////////////
+userSchema.virtual('passwordConfirmation')
+  .set(function(passwordConfirmation){
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.virtual('memberSince')
+  .get(function(){
+    return this.createdAt.getFullYear();
+  });
+
+//Pre validation hooks
+userSchema.pre('validation', function(next){
+  console.log('Prevalidation hook fired');
+  if(this.passwordConfirmation !== this.password){
+    console.log('Passwords did not match');
+    this.invalidate('passwordConfirmation', 'Does not match');
+  }
+  next();
+});
+
+//Pre save hooks
+userSchema.pre('save', function(next){
+  console.log('Pre save fired');
+  this.password = bcrypt.hashSync(this.password, 8);
+  next();
+});
+
+userSchema.post('save', function(){
+  console.log('Password was hashed', this.password);
+});
+
+
+module.exports = mongoose.model('User', userSchema);
