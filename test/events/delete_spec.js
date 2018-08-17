de14b3e9e19db28696b5e508f988443214c7415a
Sean.Rennie <sean.rennie6@gmail.com>
2018-08-17 13:34:24 +0100

    wrote the event delete tests

--- test/events/delete_spec.js
+++ test/events/delete_spec.js
@@ -1,13 +1,45 @@
-/* globals describe, it, api, expect, beforeEach */
+/* globals describe, it, xit, api, expect, beforeEach */
 
 const User = require('../../models/user');
 const Event = require('../../models/event');
 const jwt = require('jsonwebtoken');
 const { secret } = require('../../config/environment');
 
-const userData =
+const userData = {
+  email: 'rnnsea001@gmail.com',
+  firstName: 'Sean',
+  homeLocation: {
+    lat: 51.471337,
+    lon: -0.184276
+  },
+  isHost: true,
+  password: 'pass',
+  passwordConfirmation: 'pass',
+  surname: 'Rennie',
+  username: 'Rennzie'
+};
 
-const eventData =
+const eventData = [
+  {                   //Calisthenics in the park
+    category: 'body',
+    capacity: 3,
+    concluded: false,
+    duration: 60,
+    description: 'Small group Calisthenics training focussed on intermediate athletes looking to get better at handstands and muscle ups',
+    eventTitle: 'Calisthenics in the Park',
+    eventDate: 1534978800000,
+    guests: [],
+    imageUrl: 'http://www.bjj-usa.com/wp-content/uploads/2017/08/calisthenics-benefits-6.jpg',
+    isIndoors: false,
+    location: {     //sub document to hold event location
+      streetNumber: 9,
+      streetName: 'Greensward',
+      postcode: 'sw62tg',
+      lat: 51.471337,
+      lon: -0.184276          // should seed this initially
+    }
+  }
+];
 
 let token; //token is global
 let eventId;
@@ -29,7 +61,7 @@ describe('DELETE /whiskeys/:id', () => {
   });
 
 
-  it('should return a 401 without a token', done => {
+  xit('should return a 401 without a token', done => {
     api.delete(`/api/event/${eventId}`)
       .end((err, res) => {
         expect(res.status).to.eq(401);
@@ -37,7 +69,7 @@ describe('DELETE /whiskeys/:id', () => {
       });
   });
 
-  it('should return a 204 with a token', done => {
+  xit('should return a 204 with a token', done => {
     api.delete(`/api/event/${eventId}`)
       .set('Authorization', `Bearer ${token}`) // creates an authorisation header
       .end((err, res) => {
@@ -47,12 +79,12 @@ describe('DELETE /whiskeys/:id', () => {
       });
   });
 
-  it('should delete the whiskey', done => {
+  xit('should delete the whiskey', done => {
     api.delete(`/api/event/${eventId}`)
       .set('Authorization', `Bearer ${token}`) // creates an authorisation header
       .then(() => Event.find())
       .then(events => {
-        expect(whiskeys.length).to.eq(0);
+        expect(events.length).to.eq(0);
         done();
       });
   });
