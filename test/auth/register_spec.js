5eae803eddb32251893fdfce9812bc00455c29de
Sean.Rennie <sean.rennie6@gmail.com>
2018-08-17 15:02:48 +0100

    updated register tests

---
+++ test/auth/register_spec.js
@@ -0,0 +1,51 @@
+/* globals describe, it, api, expect, beforeEach */
+
+const User = require('../../models/user');
+
+//test userData
+const userData = {
+  email: 'rnnsea001@gmail.com',
+  firstName: 'Sean',
+  homeLocation: {
+    lat: 51.471337,
+    lon: -0.184276
+  },
+  isHost: true,
+  password: 'pass',
+  passwordConfirmation: 'pass',
+  surname: 'Rennie',
+  username: 'Rennzie'
+};
+
+describe('POST /api/register', () => {
+  //before each test should create a new user to test without
+  beforeEach(done => {
+    User.remove({})
+      .then(() => User.create(userData))
+      .then(() => done());
+  });
+
+  it('should recieve 201 status if user is created successfully', done => {
+    api.post('/api/register')
+      .send(userData)
+      .end((err, res) => {
+        expect(res.status).to.eq(201);
+        done();
+      });
+
+  });
+
+  // it('should return the correct data', done => {
+  //   api.post('/api/events')
+  //     //.set('Authorization', `Bearer ${token}`) // creates an authorisation header
+  //     .send(userData)
+  //     .end((err, res) => {
+  //       expect(res.body.email).to.eq(userData.email);
+  //       expect(res.body.firstName).to.eq(userData.firstName);
+  //       expect(res.body.isHost).to.eq(userData.isHost);
+  //       expect(res.body.surname).to.eq(userData.surname);
+  //       expect(res.body.username).to.eq(userData.username);
+  //       done();
+  //     });
+  // });
+});
