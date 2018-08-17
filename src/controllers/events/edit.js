364f9ebff61e4f10042d1ddb96851beabc231d83
Sophie Cornish <sophieahcornish@gmail.com>
2018-08-17 15:06:28 +0100

    post fire

--- src/controllers/events/edit.js
+++ src/controllers/events/edit.js
@@ -1,5 +1,6 @@
 function EventsEditCtrl($scope, $http, $state) {
   $scope.updateEvent = function() {
+    console.log('the event is -------->', $scope.event);
     $http({
       method: 'PUT',
       url: `/api/events/${$state.params.id}`,
