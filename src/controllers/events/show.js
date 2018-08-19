12ec3b5c36d240ad869f82db274e55db4041d300
Sean.Rennie <sean.rennie6@gmail.com>
2018-08-19 13:17:04 +0100

    can add the current user to an array which update the db

--- src/controllers/events/show.js
+++ src/controllers/events/show.js
@@ -7,6 +7,22 @@ function EventsShowCtrl($http, $state, $scope) {
       console.log('Found an event', res.data);
       $scope.event = res.data;
     });
+
+  $scope.attend = function(){
+  //make a put request to event update
+  //send an update guest array
+  //  --> this should include all required fields
+
+    const updateData = $scope.event;
+    updateData.guests.push($scope.getPayload().sub);
+
+    console.log('the updated data is: ', updateData);
+    $http({
+      method: 'PUT',
+      url: `/api/events/${$state.params.id}`,
+      data: JSON.stringify(updateData)
+    });
+  };
 }
 
-export default EventsShowCtrl; 
+export default EventsShowCtrl;
