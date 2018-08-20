43410bede6a5d0963a8f8e6e5dfac7c677020d3e
Sophie Cornish <sophieahcornish@gmail.com>
2018-08-20 17:43:08 +0100

    event map

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ import angular from 'angular';
 import '@uirouter/angularjs';
 import 'satellizer';
 import 'angular-messages';
-import './scss/style.scss';
 import 'bulma';
-
+import './scss/style.scss';
 //--------- ROUTERS------------//
 import Router from './config/routes';
 
