e2ceda249c0f076815b8f31111cfbe515e748cef
Sean.Rennie <sean.rennie6@gmail.com>
2018-08-23 13:31:03 +0100

    navbar is fixed to top and responsive

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -10,7 +10,6 @@ function MainCtrl($scope, $auth, $state, $rootScope, $timeout) {
 
   $rootScope.$on('flashMessage', (e, data) => {
     $scope.flashMessage = data;
-
     $timeout(() => $scope.flashMessage = null, 3000);
   });
 
