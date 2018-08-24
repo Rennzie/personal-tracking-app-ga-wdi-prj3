7aca4d8491fc7be18ba32b46d9c685a9272b3ead
Sean.Rennie <sean.rennie6@gmail.com>
2018-08-24 10:30:12 +0100

    displays user profile photo if logged in

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,9 +1,29 @@
 import moment from 'moment';
 
-function MainCtrl($scope, $auth, $state, $rootScope, $timeout) {
+function MainCtrl($scope,$http, $auth, $state, $rootScope, $timeout) {
   // NOTE: isAuthenticated is a function!!
   $scope.isAuthenticated = $auth.isAuthenticated; // now available in every view and controller
   $scope.getPayload = $auth.getPayload;
+
+  if($auth.isAuthenticated()){
+    getUserData();
+  }
+
+  //need to watch for a new token on login.
+  $scope.$watch('token', () => {
+    getUserData();
+  });
+
+  $scope.token = $auth.getPayload();
+
+  function getUserData(){
+    //////////--------REQUEST THE USERS INFO-------////////////
+    $http({
+      method: 'GET',
+      url: `/api/users/${$auth.getPayload().sub}`
+    })
+      .then(res => $scope.userData = res.data);
+  }
 
   $scope.currentMonth = moment().format('MMMM');
   $scope.daysRemainingInMonth = moment().daysInMonth() - moment().date();
